20f18ca05637826332e961ce00e04f835ccc0b2f
Prince of Kochi <mathenjs@gmail.com>
2020-07-19 11:23:59 -0700

    write, read, and delete notes

--- server2.js
+++ server2.js
@@ -7,7 +7,7 @@ var path = require("path");
 // var routes = require("./routes"); 
 
 
-db = [];
+notesArray = [];
 
 // Sets up the Express App
 // =============================================================
@@ -30,9 +30,52 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
 
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 })
+
+// Sets up the server routes
+// =============================================================
+
+app.get("/api/notes", function (req, res) {
+    notesArray = JSON.parse(fs.readFileSync('./db/db.json'));
+    res.json(notesArray);
+})
+
+app.post("/api/notes", function (req, res) {
+    newJournal = {
+        id: notesArray.length + Math.floor(Math.random()*100),
+        title: req.body.title,
+        text: req.body.text,
+    }
+    notesArray.push(newJournal);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesArray), function(errr) {
+        if(err) {
+            throw err
+        }
+        res.json(notesArray)
+    });
+})  
+
+app.delete("/api/notes/:id", function (req, res) {
+    var deletedNote = [];
+    for (var i = 0; i < notesArray.length; i++) {
+        if(notesArray[i].id != req.params.id) {
+            deletedNote.push(notesArray[i]);
+        }
+    }
+    notesArray = deletedNote;
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesArray), function (err){
+        if(err) {
+            throw err
+        }
+        res.json(notesArray);
+    })
+
+})
+
+
 
 // Starts the server to begin listening
 // =============================================================
