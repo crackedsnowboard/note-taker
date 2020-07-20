644967fc60236832923274bb15dba1b727fcecc8
Prince of Kochi <mathenjs@gmail.com>
2020-07-19 21:35:27 -0700

    message h10

--- server.js
+++ server.js
@@ -5,10 +5,11 @@
 // =============================================================
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 3000;
+
 
 
 // Sets up the Express app to handle data parsing
+var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"))
